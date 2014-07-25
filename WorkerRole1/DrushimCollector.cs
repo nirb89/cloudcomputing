4192ca28fd78c39577bfc0546257c64f5fb4e782
nirb <nirb89@gmail.com>
2014-07-25 15:43:39 +0300

    fixed wrong host header for Drushim

--- WorkerRole1/DrushimCollector.cs
+++ WorkerRole1/DrushimCollector.cs
@@ -24,7 +24,7 @@ namespace WorkerRole1
 
             // Download the entire HTML of the page
             WebClient webClient = new WebClient();
-            webClient.Headers[HttpRequestHeader.Host] = "www.jobmaster.co.il";
+            webClient.Headers[HttpRequestHeader.Host] = "www.drushim.co.il";
             webClient.Headers[HttpRequestHeader.UserAgent] = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36";
             string htmlCode = webClient.DownloadString(jobMasterUrl);
 
